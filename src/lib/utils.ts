4bffa47e2f7db6fce98ebae768dbf4f64f68d701
tettekete <6535634+tettekete@users.noreply.github.com>
2024-11-27 22:10:51 +0900

    add: utility functions in lib/utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,85 @@
+import * as vscode from 'vscode';
+import { execSync ,exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
+
+/**
+ * Get the Git diff for a specific file within a repository.
+ * 
+ * This function retrieves the output of the `git diff` command for a given file path,
+ * executed in the specified repository directory. If there is no difference, an empty
+ * string is returned.
+ * 
+ * @param {string} repo_dir - The path to the Git repository directory.
+ * @param {string} file_path - The path to the file to check for differences.
+ * @returns {string} The Git diff output as a string, or an empty string if there are no differences.
+ */
+export function getGitDiff( repo_dir:string , file_path: string ):string
+{
+	const stdout = execSync(
+								// `git diff --unified=0 "${file_path}"`,
+								`git diff "${file_path}"`,
+								{cwd: repo_dir }
+							).toString();
+	if( stdout.length )
+	{
+		return stdout;
+	}
+
+	return '';
+}
+
+
+/**
+ * Checks if a given directory is tracked by Git.
+ * 
+ * This function determines whether the specified directory is part of a Git repository
+ * by executing the `git rev-parse --is-inside-work-tree` command within the directory.
+ * 
+ * @async
+ * @param {string} directory - The path to the directory to check.
+ * @returns {Promise<boolean>} A promise that resolves to `true` if the directory is tracked by Git, or `false` otherwise.
+ */
+export async function isGitTrackedDir( directory: string ):Promise<boolean>
+{
+	let is_tracked = true;
+	try
+	{
+		const { stdout , stderr } = await execAsync(
+			'git rev-parse --is-inside-work-tree',
+			{ cwd: directory },
+		);
+	}
+	catch( e )
+	{
+		is_tracked = false;
+	}
+
+	return is_tracked;
+}
+
+/**
+ * Find the workspace folder that contains the given file path.
+ * @param filePath The file path to check.
+ * @returns The root path of the workspace folder, or `undefined` if not found.
+ */
+export function findWorkspaceFolder(filePath: string): string | undefined
+{
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+
+    if ( ! workspaceFolders )
+	{
+        return undefined;
+    }
+
+	for (const folder of workspaceFolders)
+	{
+		if (filePath.startsWith(folder.uri.fsPath))
+		{
+			return folder.uri.fsPath;
+		}
+	}
+
+    return undefined;
+}
