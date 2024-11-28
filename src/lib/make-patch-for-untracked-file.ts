99150d6437e867e194f1595219e1e8b879475d5e
tettekete <6535634+tettekete@users.noreply.github.com>
2024-11-28 15:20:47 +0900

    feat: implement main function to create patches for untracked files
    - Added a main function dedicated to generating patches for untracked files.

---
+++ src/lib/make-patch-for-untracked-file.ts
@@ -0,0 +1,60 @@
+import * as vscode from 'vscode';
+import { PatchMaker } from './patch-maker';
+import { LineRange } from './line-range';
+import { AnyLineChange ,AddedLine } from 'parse-git-diff';
+
+export function makePatchForUntrackedFile(
+	{
+		editor,
+		rel_file_path
+	}:
+	{
+		editor: vscode.TextEditor;
+		rel_file_path: string
+	}
+)
+{
+	const document = editor.document;
+	const selection = editor.selection;
+
+	let startLine:number;
+	let endLine:number;
+
+	if ( selection.isEmpty )
+	{
+		const cursorPosition = selection.active;
+		startLine = endLine = cursorPosition.line;
+	}
+	else
+	{
+		startLine	= selection.start.line;
+		endLine		= selection.end.line;
+	}
+
+	const selectionRange = new LineRange( startLine , endLine );
+	selectionRange.offsetRange( -startLine );
+	const zeroRange	= new LineRange( 0,0 );
+	zeroRange.linesAdjustment = 0;
+
+	const patchMaker = new PatchMaker({
+		from_file: '/dev/null',
+		omit_a_prefix: true,
+		to_file: rel_file_path,
+		from_range: zeroRange,
+		to_range: selectionRange
+	});
+
+	for(let i=startLine;i<=endLine;i++)
+	{
+		const change: AddedLine =
+		{
+			type: 'AddedLine',
+			lineAfter: i + 1,
+			content: document.lineAt( i ).text
+		};
+
+		patchMaker.pushChanges( change );
+	}
+
+	return patchMaker.toString();
+}
